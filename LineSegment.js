6164bcfaefb7e207e7161569b34d9c2121845ca0
aw449 <awong7905@gmail.com>
2015-02-06 17:02:52 -0500

    Log data

--- LineSegment.js
+++ LineSegment.js
@@ -7,3 +7,5 @@ client.takeoff()
 	.after(3000, function() {this.front(0.7); })
 	.after(3000, function() {this.stop();     })
 	.after(3000, function() {this.land();     });
+	//Creates data log
+	client.on('navdata', console.log);
