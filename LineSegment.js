e4cdfb213b3854c9e939e99dfee292333b62be8e
Ankeet Parikh <ankeet@nbp-151-232.nbp.ruw.rutgers.edu>
2015-02-06 17:00:38 -0500

    Fixed timings

--- LineSegment.js
+++ LineSegment.js
@@ -3,7 +3,7 @@ var client = ardrone.createClient();
 
 client.takeoff()
 	
-client.after(3000, function() {this.stop();     })
-	.after(3000, function() {this.front(0.7); })
+client.after(2000, function() {this.stop();     })
+	.after(1000, function() {this.front(0.7); })
 	.after(3000, function() {this.stop();     })
-	.after(3000, function() {this.land();     });
+	.after(2000, function() {this.land();     });
