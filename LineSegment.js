d1f0bcab77de8f44f704670b570388859c031e4e
Ankeet Parikh <ankeet@nbp-151-232.nbp.ruw.rutgers.edu>
2015-02-06 17:05:37 -0500

    Added navdata line

--- LineSegment.js
+++ LineSegment.js
@@ -1,17 +1,9 @@
 var ardrone = require('ar-drone');
 var client = ardrone.createClient();
-
+client.on('navdata', console.log);
 client.takeoff()
 	
 client.after(2000, function() {this.stop();     })
 	.after(1000, function() {this.front(0.7); })
 	.after(3000, function() {this.stop();     })
-<<<<<<< HEAD
-	.after(3000, function() {this.front(0.7); })
-	.after(3000, function() {this.stop();     })
-	.after(3000, function() {this.land();     });
-	//Creates data log
-	client.on('navdata', console.log);
-=======
 	.after(2000, function() {this.land();     });
->>>>>>> origin/master
