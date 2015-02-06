9b0f1300ba53318f6a77ec9b75a1e48e0e8b8edd
Ankeet Parikh <ankeet@nbp-151-232.nbp.ruw.rutgers.edu>
2015-02-06 16:35:20 -0500

    LineSegment test1

---
+++ LineSegment.js
@@ -0,0 +1,9 @@
+var ardrone = require('ar-drone');
+var client = ardrone.createClient();
+
+client.takeoff()
+	
+	.after(3000, function() {this.stop();     })
+	.after(3000, function() {this.front(0.7); })
+	.after(3000, function() {this.stop();     })
+	.after(3000, function() {this.land();     });
