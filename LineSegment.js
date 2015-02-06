e9eb76d9f047f77c0db55b0c092c9bef43cd7cb3
Ankeet Parikh <ankeet@nbp-151-232.nbp.ruw.rutgers.edu>
2015-02-06 17:09:02 -0500

    Linesegfix

--- LineSegment.js
+++ LineSegment.js
@@ -3,7 +3,7 @@ var client = ardrone.createClient();
 client.on('navdata', console.log);
 client.takeoff()
 	
-client.after(2000, function() {this.stop();     })
-	.after(1000, function() {this.front(0.7); })
-	.after(3000, function() {this.stop();     })
-	.after(2000, function() {this.land();     });
+client.after(2000, function() {this.stop();      })
+	.after(1000, function() {this.front(0.7);    })
+	.after(3000, function() {this.stop();        })
+	.after(2000, function() {this.land();        });
